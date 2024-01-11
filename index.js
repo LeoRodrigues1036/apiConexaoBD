3a391b1d2b87d66ded9184944ff1bed010c96a7a
Leonardo Rodrigues <leorodrigues1036@gmail.com>
2024-01-11 19:11:02 -0300

    novo script

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+const express = require('express');
 const { Pool } = require('pg');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-pool.query('SELECT * FROM sua_tabela', (err, res) => {
-  if (err) {
-    console.error('Erro ao executar a consulta', err);
-  } else {
-    console.log('Resultado da consulta:', res.rows);
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM produtos');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Erro ao executar a consulta', error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
   }
-  // Encerrar a conexão após a consulta
-  pool.end();
+});
+
+// Outras rotas podem ser adicionadas conforme necessário
+
+app.listen(port, () => {
+  console.log(`Servidor está rodando na porta ${port}`);
 });
