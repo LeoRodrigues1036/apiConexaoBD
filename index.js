383fab6c8dbb79baee57e9cb42fe1e399a7b19dd
Leonardo Rodrigues <leorodrigues1036@gmail.com>
2024-01-11 19:03:48 -0300

    novo script

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const { Pool } = require('pg');
 
-const connectionString = 'postgresql://LeoRodrigues1036:************@ep-winter-cell-78315372.us-east-2.aws.neon.tech/BancoDeDados?sslmode=require';
-
 const pool = new Pool({
-  connectionString: connectionString,
+  connectionString: process.env.DATABASE_URL,
 });
 
-// Agora você pode usar o objeto `pool` para realizar consultas.
-pool.query('SELECT * FROM produtos', (err, res) => {
+pool.query('SELECT * FROM sua_tabela', (err, res) => {
   if (err) {
     console.error('Erro ao executar a consulta', err);
   } else {
